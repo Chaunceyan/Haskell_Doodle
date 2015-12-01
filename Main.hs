130d48c3ef88255d24dcfb81be2fdbd4e0991f6c
chauncey <Chauncey.ycx@gmail.com>
2015-12-02 00:13:28 +0100

    Finished Data Defination

--- Main.hs
+++ Main.hs
@@ -3,14 +3,25 @@ import Doodle (Doodle(initialize, add, remove, toogle), Pool(freshKey, get, set)
 -- Data for time slots in Doodle 
 data Slot = Slot { startTime :: String
                  , endTime :: String
-                 , participants [String]} deriving Show
+                 , participants [String] } deriving Show
 
--- Data for titles
-data Title id  = Title { title :: String
-                   , slots [Slot]
-                   , id :: id}
-instance Doodle emptyDoodle where
-    initialize = 
+-- Data and Instance for MyDoodle
+data MyDoodle id  = MyDoodle { title :: String
+                   , slots [Slot] }
 
-main = run emptyDoodle
+instance Doodle MyDoodle where
+    initialize newTitle = MyDoodle { title = newTitle
+                                   , slots = [] 
+                                   , participants = [] }
+    add slot = slots : slot
+    remove participate = participants index participants 
+
+-- Data and Instance for Pool
+data MyPool = MyPool { indentifiers = [1..]
+                     , pairs = [(Int, MyDoodle)] }
+
+instance Pool MyPool where
+    freshKey MyPool = key (MyDoodle) 
+    get key = 
+
 
